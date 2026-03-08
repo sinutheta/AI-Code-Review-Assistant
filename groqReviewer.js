3eb904ab82204b00d6fc41dbb5601e757dfdda49
sinjini <sinjinidatta27@gmail.com>
2026-03-08 13:22:25 +0530

    Refactor groqReviewer.js to use Express for API

---
+++ groqReviewer.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const reviewCode = require("./groqReviewer");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../FrontEnd")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../FrontEnd/index.html"));
+});
+
+app.post("/review", async (req, res) => {
+  try {
+    const { code } = req.body;
+    const result = await reviewCode(code);
+    res.json({ analysis: result });
+  } catch (err) {
+    res.status(500).json({ error: "AI analysis failed." });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
